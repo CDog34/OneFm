082ae66e530a47af91b15357bd136ac858cb9da4
Peter Cao <i@cdog.me>
2015-10-05 23:28:49 +0800

    233

--- js/js.js
+++ js/js.js
@@ -20,6 +20,11 @@ $cdfm.hash=[];
 $cdfm.coverImg=document.getElementById("cover-img");
 $cdfm.idIn=document.getElementById("id-in");
 $cdfm.so=document.getElementById("so");
+$cdfm.oldHash="/";
+$cdfm.curPlaying=0;
+$cdfm.forwordBtn=document.getElementById("ctrl-fwd");
+$cdfm.backwordBtn=document.getElementById("ctrl-bwd");
+
 
 
 window.onload=function(){
@@ -33,7 +38,7 @@ window.onload=function(){
 
     window.onhashchange=$cdfm.hashChange;
 
-    $cdfm.cdp.oncanplay=$cdfm.ready2Play;
+    $cdfm.cdp.oncanplaythrough=$cdfm.ready2Play;
 
     //$cdfm.cdp.ontimeupdate=$cdfm.updateTime;
     setInterval(function () {
@@ -45,38 +50,81 @@ window.onload=function(){
     $cdfm.playPauseBtn.disabled=true;
 
     $cdfm.playPauseBtn.onclick=$cdfm.playPause;
+    $cdfm.forwordBtn.onclick=$cdfm.forword;
+    $cdfm.backwordBtn.onclick=$cdfm.backword;
     $cdfm.so.onclick=$cdfm.doso;
+
+    $cdfm.cdp.onended=$cdfm.forword;
 
 
 };
 $cdfm.hashChange=function () {
-    $cdfm.playPause(false);
     $cdfm.hash=location.hash.substr(2).split("/");
-    if ($cdfm.hash[0]=="song"){
-        $cd.ajax({
-            url:"http://m163.proxy.izhai.net/api/song/detail/",
-            data:{
-                ids:"["+$cdfm.hash[1]+"]"
-            },
-            success:function(txt,xml){
-                $cdfm.vData=JSON.parse(txt);
-                $cdfm.newSong();
-            }
-        })
+    console.log($cdfm.oldHash,location.hash);
+    if($cdfm.oldHash!=location.hash){
+        $cdfm.playPause(false);
+
+        if ($cdfm.hash[0]=="song"){
+            $cd.ajax({
+                url:"http://m163.proxy.izhai.net/api/song/detail/",
+                data:{
+                    ids:"["+$cdfm.hash[1]+"]"
+                },
+                success:function(txt,xml){
+                    var json=JSON.parse(txt);
+                    if (json.code==200&&json.songs.length>0){
+                        $cdfm.oldHash=location.hash;
+                        $cdfm.vData=json;
+                        $cdfm.curPlaying=0;
+                        $cdfm.newSong();
+                    }else{
+                        location.hash="/list/"+$cdfm.hash[1];
+                        $cdfm.playPause(true);
+                    }
+
+                }
+            })
+        }else if ($cdfm.hash[0]=="list"){
+            $cd.ajax({
+                url:"http://m163.proxy.izhai.net/api/playlist/detail/",
+                data:{
+                    id:$cdfm.hash[1]
+                },
+                success:function(txt,xml){
+                    var json=JSON.parse(txt);
+                    if (json.code==200){
+                        $cdfm.vData={songs:[]};
+                        for (var i in json.result.tracks){
+                            $cdfm.vData.songs.push(json.result.tracks[i]);
+                        }
+                        $cdfm.oldHash=location.hash;
+                        $cdfm.curPlaying=0;
+                        $cdfm.newSong();
+                    }else{
+                        location.hash=$cdfm.oldHash;
+                        $cdfm.playPause(true);
+                    }
+
+                }
+            })
+        }else{
+            location.hash="/list/89768862";
+        }
     }
+
 }
 $cdfm.newSong=function(){
     console.log("Preparing a new song");
-    $cdfm.cdp.src=$cdfm.vData.songs[0].mp3Url;
-    $cdfm.title.innerHTML=$cdfm.vData.songs[0].name;
-    if ($cdfm.vData.songs[0].alias[0]){
-        $cdfm.subTitle.innerHTML=$cdfm.vData.songs[0].alias[0];
+    $cdfm.cdp.src=$cdfm.vData.songs[$cdfm.curPlaying].mp3Url;
+    $cdfm.title.innerHTML=$cdfm.vData.songs[$cdfm.curPlaying].name;
+    if ($cdfm.vData.songs[$cdfm.curPlaying].alias[0]){
+        $cdfm.subTitle.innerHTML=$cdfm.vData.songs[$cdfm.curPlaying].alias[0];
     }else{
         $cdfm.subTitle.innerHTML="";
     }
-    $cdfm.artist.innerHTML=$cdfm.vData.songs[0].artists[0].name;
-    $cdfm.cover.style.backgroundImage="url("+$cdfm.vData.songs[0].album.picUrl+")";
-    $cdfm.coverImg.src=$cdfm.vData.songs[0].album.picUrl;
+    $cdfm.artist.innerHTML=$cdfm.vData.songs[$cdfm.curPlaying].artists[0].name;
+    $cdfm.cover.style.backgroundImage="url("+$cdfm.vData.songs[$cdfm.curPlaying].album.picUrl+")";
+    $cdfm.coverImg.src=$cdfm.vData.songs[$cdfm.curPlaying].album.picUrl;
     $cdfm.cdp.load();
     $cdfm.playPauseBtn.className="fa fa-play";
 
@@ -127,7 +175,23 @@ $cdfm.doso=function(){
         location.hash="/song/"+$cdfm.idIn.value;
         $cdfm.idIn.value="";
     }
+}
+$cdfm.forword=function(){
+    if (++$cdfm.curPlaying<$cdfm.vData.songs.length){
+        $cdfm.newSong();
+    }else{
+        $cdfm.curPlaying=0;
+        $cdfm.newSong();
+    }
 
+}
+$cdfm.backword=function(){
+    if (--$cdfm.curPlaying>=0){
+        $cdfm.newSong();
+    }else{
+        $cdfm.curPlaying=0;
+        $cdfm.newSong();
+    }
 
 }
 
